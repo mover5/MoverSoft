2bc4b541e68324b75b87c72474a0107dcaacee4e
Mark <moverholt@outlook.com>
2015-12-10 23:14:33 -0800

    Updated nuget ingo

--- MoverSoft.Common/Properties/AssemblyInfo.cs
+++ MoverSoft.Common/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MoverSoft.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A common library of simple helpers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mark Overholt")]
 [assembly: AssemblyProduct("MoverSoft.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
--- MoverSoft.StorageLibrary/Properties/AssemblyInfo.cs
+++ MoverSoft.StorageLibrary/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MoverSoft.StorageLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library for working with Azure Storage")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mark Overholt")]
 [assembly: AssemblyProduct("MoverSoft.StorageLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
